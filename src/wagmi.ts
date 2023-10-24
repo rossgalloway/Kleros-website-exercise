855c104005a43d68e7bf22823a23455556a13f64
Ross <therossgalloway@gmail.com>
2023-10-24 11:32:13 -0400

    Initial commit from create-wagmi

---
+++ src/wagmi.ts
@@ -0,0 +1,28 @@
+import { getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createConfig } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+
+const walletConnectProjectId = '9b7017758215799617937561658d838e'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
+  [
+    publicProvider(),
+  ],
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'My wagmi + RainbowKit App',
+  chains,
+  projectId: walletConnectProjectId,
+})
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+  webSocketPublicClient,
+})
+
+export { chains }
